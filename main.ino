4f4eff2a3ae3e5df57af4f797c351317f1120610
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2023-10-05 11:51:06 +0200

    initial commit

---
+++ main.ino
@@ -0,0 +1,15 @@
+// phases: 
+// - game phase:
+//   - led sequence
+//   - button sequence
+//   - end round phase
+// - waiting for game
+// - sleep fase
+
+void setup() { 
+  Serial.begin(115200);
+}
+
+void loop() {
+ 
+}
