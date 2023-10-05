8b8319821f53a246db2fb34eba197ad351354b4a
Riccardo Gardenghi <riccardo.gardenghi@antreem.it>
2023-10-05 11:59:30 +0200

    add phase

--- main.ino
+++ main.ino
@@ -6,10 +6,32 @@
 // - waiting for game
 // - sleep fase
 
+enum PHASE {
+  waiting_for_game,
+  game_phase,
+  sleep_phase
+};
+
+PHASE currentPhase = waiting_for_game;
+
 void setup() { 
   Serial.begin(115200);
 }
 
 void loop() {
- 
+ switch (currentPhase)
+ {
+ case waiting_for_game:
+  /* code */
+  break;
+ case game_phase:
+  /* code */
+  break;
+ case sleep_phase:
+  /* code */
+  break;
+ default:
+  //reset
+  break;
+ }
 }
