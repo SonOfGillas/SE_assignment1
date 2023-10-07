a323fabc7960bfb7e327db5d7a49effee69b281a
as3ii <as3ii777@gmail.com>
2023-10-07 16:18:34 +0200

    Fixed various typos

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,8 +1,8 @@
-// phases: 
+// phases:
 // - game:
 //   - led sequence
 //   - button_ sequence
-//   - end round 
+//   - end round
 // - waiting for game:
 //   - fade led
 //   - sleep
@@ -14,9 +14,9 @@
 // led pin
 #define LED_RED 10
 #define LED_GREEN_1 12
-#define lED_GREEN_2 11
-#define lED_GREEN_3 9
-#define lED_GREEN_4 8
+#define LED_GREEN_2 11
+#define LED_GREEN_3 9
+#define LED_GREEN_4 8
 
 // button_ pin
 #define BUTTON_1 7
@@ -24,28 +24,28 @@
 #define BUTTON_3 5
 #define BUTTON_4 4
 
-// potenziometer
+// potentiometer
 #define POT A0
 
 PHASE currentPhase = waiting;
 bool isPhaseChanged = false;
 STATUS phaseStatus = ok;
-const int leds[] = {LED_GREEN_1,lED_GREEN_2,lED_GREEN_3,lED_GREEN_4};
+const int leds[] = {LED_GREEN_1, LED_GREEN_2, LED_GREEN_3, LED_GREEN_4};
 
 void setup() {
   // led initialization
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN_1, OUTPUT);
-  pinMode(lED_GREEN_2, OUTPUT);
-  pinMode(lED_GREEN_3, OUTPUT);
-  pinMode(lED_GREEN_4, OUTPUT);
-  // button initializzation
+  pinMode(LED_GREEN_2, OUTPUT);
+  pinMode(LED_GREEN_3, OUTPUT);
+  pinMode(LED_GREEN_4, OUTPUT);
+  // button initialization
   pinMode(BUTTON_1, INPUT_PULLUP);
   pinMode(BUTTON_2, INPUT_PULLUP);
   pinMode(BUTTON_3, INPUT_PULLUP);
   pinMode(BUTTON_4, INPUT_PULLUP);
   //pot
-  pinMode(POT,INPUT);
+  pinMode(POT, INPUT);
 
   Serial.begin(115200);
 }
@@ -71,14 +71,14 @@ void runPhase(){
 }
 
 void checkPhaseSwitch(){
-  if(phaseStatus = go_next_phase){
+  if(phaseStatus == go_next_phase){
     if(currentPhase == waiting){
       currentPhase = game;
     } else {
       currentPhase = waiting;
     }
-    isPhaseChanged == true;
+    isPhaseChanged = true;
   } else {
-    isPhaseChanged == false;
+    isPhaseChanged = false;
   }
 }
