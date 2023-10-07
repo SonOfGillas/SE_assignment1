2c7e27ef6e70d63120ea32c33c0fc1ec3b0a1843
riccardo gardenghi <richy.gardenghi@gmail.com>
2023-10-07 17:28:01 +0200

    show_sequence

--- src/main/game_phase.cpp
+++ src/main/game_phase.cpp
@@ -1,15 +1,51 @@
 #include "game_phase.h"
+#include <bits/stdc++.h>
 
 GAME_SUBPHASE currentGameSubPhase = show_sequence;
 STATUS gameSubPhaseStatus = ok;
-bool isGameSubPhaseChanged = false;
 bool gameOver = false;
+long gameStartingTime;
+int sequence[NUM_GREEN_LEDS];
+
+int T1 = 2000; //pause between match
+int T2 = 1000; //pause between one led of the sequence turn off
+int T3 = 5000; //response time
+
 namespace game_phase {
   static void init() {
     currentGameSubPhase = show_sequence;
+    gameStartingTime=millis();
   }
 
+  void shuffle_array(int arr[], int n)
+    {
+        unsigned seed = gameStartingTime;
+
+        // Shuffling our array
+        shuffle(arr, arr + n,
+                default_random_engine(seed));
+  
+    }
+
   static STATUS showSequence() {
+    for(int led : leds)
+      digitalWrite(led, HIGH);
+    
+    delay(T1);
+
+    // copy leds into sequence
+    for(int i=0;i<NUM_GREEN_LEDS;i++){
+      sequence[i] = leds[i];
+    }
+
+    // shuffle sequence
+    int n = sizeof(sequence) / sizeof(sequence[0]);
+    shuffle_array(sequence, n);
+
+    //better shuffle ?
+    random_shuffle(std::begin(a), std::end(a));
+    
+
     return ok;
   }
 
@@ -49,9 +85,6 @@ namespace game_phase {
       } else {
         currentGameSubPhase = static_cast<GAME_SUBPHASE>(static_cast<int>(currentGameSubPhase) + 1);
       }
-      isGameSubPhaseChanged = true;
-    } else {
-      isGameSubPhaseChanged = false;
     }
   }
 
