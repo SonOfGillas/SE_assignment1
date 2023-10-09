ace8f75a87766c38c6e0483936c8b258aeb0effc
as3ii <as3ii777@gmail.com>
2023-10-09 19:29:45 +0200

    updated circuit to assign the peripherals to the right pins, fixed some typos

--- src/main/model.h
+++ src/main/model.h
@@ -1,20 +1,20 @@
 #ifndef _MODEL_
-#define _MODEL_  
+#define _MODEL_
 
 // led pin
-#define LED_RED 10
+#define LED_RED 11
 #define NUM_GREEN_LEDS 4
-#define LED_GREEN_1 12
-#define LED_GREEN_2 11
-#define LED_GREEN_3 9
-#define LED_GREEN_4 8
+#define LED_GREEN_1 8
+#define LED_GREEN_2 9
+#define LED_GREEN_3 10
+#define LED_GREEN_4 12
 
 // button_ pin
 #define NUM_BUTTONS NUM_GREEN_LEDS
-#define BUTTON_1 7
-#define BUTTON_2 6
-#define BUTTON_3 5
-#define BUTTON_4 4
+#define BUTTON_1 2
+#define BUTTON_2 3
+#define BUTTON_3 4
+#define BUTTON_4 5
 
 // potentiometer
 #define POT A0
@@ -22,9 +22,9 @@
 const int leds[NUM_GREEN_LEDS] = {LED_GREEN_1, LED_GREEN_2, LED_GREEN_3, LED_GREEN_4};
 const int buttons[NUM_BUTTONS] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4};
 
-const int T1 = 2000; //pause between match
-const int T2 = 1000; //pause between one led of the sequence turn off
-const int T3 = 5000; //response time
+const int T1 = 2000; // pause between match
+const int T2 = 1000; // pause between one led of the sequence turn off
+const int T3 = 5000; // response time
 
 enum PHASE {
   waiting,
@@ -43,7 +43,7 @@ enum STATUS {
   err
 };
 
-//this value rapresent the percentage descrising of the times: T1, T2, T3
+// this value represent the percentage decreasing of the times: T1, T2, T3
 enum DIFFICULTY {
   easy = 5,
   medium = 10,
